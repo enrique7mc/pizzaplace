84215b331155b7349d438fa5646c6a12cb5b1672
Enrique Munguía <enrique7mc@gmail.com>
2017-04-09 20:57:05 -0700

    02-08

--- main.js
+++ main.js
@@ -2,15 +2,18 @@
   'use strict';
 
   var App = window.App;
+  var Order = App.Order;
+  var Store = App.Store;
   var DataStore = App.DataStore;
 
   var dataStore = new DataStore();
-  dataStore.add('valor1', 'esta es una cadena');
-  dataStore.add('valor2', 10);
-  dataStore.add('abc', 'javascript');
+  var myStore = new Store('ncc-1701', new DataStore());
+  window.myStore = myStore;
 
-  console.log(dataStore.get('valor1'));
-  console.log(dataStore.get('valor2'));
-  console.log(dataStore.get('abc'));
-  console.log(dataStore.getAll());
+  var order = new Order('enrique@devcode.com', 'large', 'cheese');
+  myStore.createOrder(order);
+  myStore.printOrders();
+
+  myStore.deliverOrder('enrique@devcode.com');
+  myStore.printOrders();
 }(window));
--- store.js
+++ store.js
@@ -7,6 +7,31 @@
     this.db = db; // composition
   }
 
+  Store.prototype.createOrder = function (order) {
+    console.log('Adding order for ' + order.email);
+    this.db.add(order.email, order);
+  };
+
+  Store.prototype.deliverOrder = function (email) {
+    console.log('Delivering order for ' + email);
+    this.db.remove(email);
+  };
+
+  Store.prototype.printOrders = function () {
+    var customerEmails = Object.keys(this.db.getAll());
+
+    console.log('Store #' + this.storeId + ' has pending orders:');
+
+    /*var self = this;
+    customerEmails.forEach(function (email) {
+      console.log(self.db.get(email));
+    });*/
+
+    customerEmails.forEach(function (email) {
+      console.log(this.db.get(email));
+    }.bind(this));
+  };
+
   App.Store = Store;
   window.App = App;
-})(window);
+}(window));
