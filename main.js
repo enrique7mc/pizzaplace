1b531ae60dbe5cb676a5285b5b4cd2357e95054f
Enrique Munguía <enrique7mc@gmail.com>
2017-04-09 20:33:23 -0700

    02-04

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ console.log('main');
 var dataStore = new DataStore();
 dataStore.add('valor1', 'esta es una cadena');
 dataStore.add('valor2', 10);
+dataStore.add('abc', 'javascript');
 console.log(dataStore);
+
+console.log(dataStore.get('valor1'));
+console.log(dataStore.get('valor2'));
+console.log(dataStore.get('abc'));
+console.log(dataStore.getAll());
+
+// Explain the relationship between these two properties
+// console.console.log(DataStore.prototype === dataStore.__proto__);
