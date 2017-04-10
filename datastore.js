1511dd7316ba9729bd504d012bbf30bb6b6815e8
Enrique Munguía <enrique7mc@gmail.com>
2017-04-09 20:40:10 -0700

    02-05

--- datastore.js
+++ datastore.js
@@ -1,19 +1,28 @@
-function DataStore() {
-  this.data = {};
-}
+(function (window) {
+  'use strict';
 
-DataStore.prototype.add = function (key, val) {
-  this.data[key] = val;
-};
+  var App = window.App || {};
 
-DataStore.prototype.get = function (key) {
-  return this.data[key];
-};
+  function DataStore() {
+    this.data = {};
+  }
 
-DataStore.prototype.getAll = function () {
-  return this.data;
-};
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  };
 
-DataStore.prototype.remove = function (key) {
-  delete this.data[key];
-};
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  };
+
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  };
+
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
+  };
+
+  App.DataStore = DataStore;
+  window.App = App;
+}(window));
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-console.log('main');
-var dataStore = new DataStore();
-dataStore.add('valor1', 'esta es una cadena');
-dataStore.add('valor2', 10);
-dataStore.add('abc', 'javascript');
-console.log(dataStore);
+(function (window) {
+  'use strict';
 
-console.log(dataStore.get('valor1'));
-console.log(dataStore.get('valor2'));
-console.log(dataStore.get('abc'));
-console.log(dataStore.getAll());
+  var App = window.App;
+  var DataStore = App.DataStore;
 
-// Explain the relationship between these two properties
-// console.console.log(DataStore.prototype === dataStore.__proto__);
+  var dataStore = new DataStore();
+  dataStore.add('valor1', 'esta es una cadena');
+  dataStore.add('valor2', 10);
+  dataStore.add('abc', 'javascript');
+
+  console.log(dataStore.get('valor1'));
+  console.log(dataStore.get('valor2'));
+  console.log(dataStore.get('abc'));
+  console.log(dataStore.getAll());
+}(window));
