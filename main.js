c36a4cd49929d013a76379c51c1de1f12a4ab408
Enrique Munguía <enrique7mc@gmail.com>
2017-04-12 20:38:30 -0700

    inheritance with objects

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var App = window.App;
-  var PizzaOrder = App.PizzaOrder;
+  var pizzaOrderFactory = App.pizzaOrderFactory;
   var Store = App.Store;
   var DataStore = App.DataStore;
 
@@ -10,7 +10,7 @@
   var myStore = new Store('ncc-1701', new DataStore());
   window.myStore = myStore;
 
-  var order = new PizzaOrder('enrique@devcode.com', 'large', 'cheese');
+  var order = pizzaOrderFactory('enrique@devcode.com', 'large', 'cheese');
   order.displayOrder(); // inherited method
   myStore.createOrder(order);
   myStore.printOrders();
